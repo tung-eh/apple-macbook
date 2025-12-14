c274405f17a7cb40ae2d1e8ad61a19f8b9f5847d
Tung Teng <thetungctn@gmail.com>
2025-12-14 16:39:06 +0700

    Register ScrollTrigger (gsap) globally

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,6 +1,11 @@
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+
 import Navbar from './Navbar'
 import Hero from './Hero'
 import ProductViewer from './ProductViewer'
+
+gsap.registerPlugin(ScrollTrigger)
 
 function App() {
   return (
