a573107929b986f4a4776e35952c350dd01197f1
Tung Teng <thetungctn@gmail.com>
2025-12-14 14:29:25 +0700

    Product viewer section content

---
+++ src/App/ProductViewer.tsx
@@ -0,0 +1,59 @@
+import { useState } from 'react'
+import clsx from 'clsx'
+
+const ProductViewer = () => {
+  const [color, setColor] = useState('#adb5bd')
+  const [scale, setScale] = useState(0.06)
+
+  return (
+    <section id="product-viewer">
+      <h2>Take a closer look.</h2>
+
+      <div className="controls">
+        <div className="flex-center gap-5 mt-5">
+          <div className="color-control">
+            <div
+              onClick={() => setColor('#adb5bd')}
+              className={clsx(
+                'bg-neutral-300',
+                color === '#adb5bd' && 'active'
+              )}
+            />
+            <div
+              onClick={() => setColor('#2e2c2e')}
+              className={clsx(
+                'bg-neutral-900',
+                color === '#2e2c2e' && 'active'
+              )}
+            />
+          </div>
+
+          <div className="size-control">
+            <div
+              onClick={() => setScale(0.06)}
+              className={clsx(
+                scale === 0.06
+                  ? 'bg-white text-black'
+                  : 'bg-transparent text-white'
+              )}
+            >
+              <p>14"</p>
+            </div>
+            <div
+              onClick={() => setScale(0.08)}
+              className={clsx(
+                scale === 0.08
+                  ? 'bg-white text-black'
+                  : 'bg-transparent text-white'
+              )}
+            >
+              <p>16"</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default ProductViewer
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,11 +1,13 @@
 import Navbar from './Navbar'
 import Hero from './Hero'
+import ProductViewer from './ProductViewer'
 
 function App() {
   return (
     <main>
       <Navbar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 }
