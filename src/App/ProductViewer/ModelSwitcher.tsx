008040bbe6022d00364ac6c38c19eb8f3b2f4917
Tung Teng <thetungctn@gmail.com>
2025-12-14 17:51:37 +0700

    Handle switch Macbook size

--- src/App/ProductViewer/ModelSwitcher.tsx
+++ src/App/ProductViewer/ModelSwitcher.tsx
@@ -1,8 +1,33 @@
+import { useRef } from 'react'
+import * as THREE from 'three'
 import { PresentationControls } from '@react-three/drei'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
 import { useMediaQuery } from 'react-responsive'
 
 import type { MacbookStore } from './store'
 import Macbook14 from './Macbook14'
+import Macbook16 from './Macbook16'
+
+const ANIMATION_DURATION = 1
+const OFFSET_DISTANCE = 5
+
+const fadeMeshes = (group: THREE.Group | null, opacity: number) => {
+  if (!group) return
+
+  group.traverse((child) => {
+    if (child instanceof THREE.Mesh) {
+      child.material.transparent = true
+      gsap.to(child.material, { opacity, duration: ANIMATION_DURATION })
+    }
+  })
+}
+
+const moveGroup = (group: THREE.Group | null, x: number) => {
+  if (!group) return
+
+  gsap.to(group.position, { x, duration: ANIMATION_DURATION })
+}
 
 const controlsConfig = {
   snap: true,
@@ -13,16 +38,39 @@ const controlsConfig = {
 }
 
 const ModelSwitcher = ({ scale }: { scale: MacbookStore['scale'] }) => {
+  const smallMacbookRef = useRef<THREE.Group>(null)
+  const largeMacbookRef = useRef<THREE.Group>(null)
   const isMobile = useMediaQuery({ query: '(max-width: 1024px)' })
   const scaleMap = isMobile
     ? { large: 0.05, small: 0.03 }
     : { large: 0.08, small: 0.06 }
 
+  useGSAP(() => {
+    if (scale === 'large') {
+      moveGroup(smallMacbookRef.current, -OFFSET_DISTANCE)
+      moveGroup(largeMacbookRef.current, 0)
+
+      fadeMeshes(smallMacbookRef.current, 0)
+      fadeMeshes(largeMacbookRef.current, 1)
+    } else {
+      moveGroup(smallMacbookRef.current, 0)
+      moveGroup(largeMacbookRef.current, OFFSET_DISTANCE)
+
+      fadeMeshes(smallMacbookRef.current, 1)
+      fadeMeshes(largeMacbookRef.current, 0)
+    }
+  }, [scale])
+
   return (
     <>
       <PresentationControls {...controlsConfig}>
-        <group>
-          <Macbook14 scale={scaleMap[scale]} />
+        <group ref={largeMacbookRef}>
+          <Macbook16 scale={scaleMap.large} />
+        </group>
+      </PresentationControls>
+      <PresentationControls {...controlsConfig}>
+        <group ref={smallMacbookRef}>
+          <Macbook14 scale={scaleMap.small} />
         </group>
       </PresentationControls>
     </>
