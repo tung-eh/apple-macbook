0959aac815fcd50f3be52972ea6760ed1c911492
Tung Teng <thetungctn@gmail.com>
2025-12-14 16:21:01 +0700

    Add spot lights
    To better seeing model edges

--- src/App/ProductViewer/StudioLights.tsx
+++ src/App/ProductViewer/StudioLights.tsx
@@ -21,6 +21,24 @@ const StudioLights = () => {
           />
         </group>
       </Environment>
+      <spotLight
+        position={[-2, 10, 5]}
+        angle={0.15}
+        decay={0}
+        intensity={Math.PI * 0.2}
+      />
+      <spotLight
+        position={[0, -25, 10]}
+        angle={0.15}
+        decay={0}
+        intensity={Math.PI * 0.2}
+      />
+      <spotLight
+        position={[0, 15, 5]}
+        angle={0.15}
+        decay={0.1}
+        intensity={Math.PI * 1}
+      />
     </group>
   )
 }
