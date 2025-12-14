db0afc46b8a7719e2cadffb88b25c6a41a97dbfa
Tung Teng <thetungctn@gmail.com>
2025-12-14 13:17:26 +0700

    Finish Navbar

--- src/App/Navbar.tsx
+++ src/App/Navbar.tsx
@@ -1,5 +1,36 @@
-const Navbar = () => {
-  return <div>Navbar</div>
-}
+const navLinks = [
+  { label: 'Store' },
+  { label: 'Mac' },
+  { label: 'iPhone' },
+  { label: 'Watch' },
+  { label: 'Vision' },
+  { label: 'AirPods' },
+]
 
-export default Navbar
+const NavBar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="Apple logo" />
+
+        <ul>
+          {navLinks.map(({ label }) => (
+            <li key={label}>
+              <a href={label}>{label}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex-center gap-3">
+          <button>
+            <img src="/search.svg" alt="Search" />
+          </button>
+          <button>
+            <img src="/cart.svg" alt="Cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  )
+}
+export default NavBar
