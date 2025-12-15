c22468bf0e09945526fc5e4aeac6f556487b4c2a
Tung Teng <thetungctn@gmail.com>
2025-12-15 10:41:45 +0700

    Showcase section content

--- src/App/Showcase.tsx
+++ src/App/Showcase.tsx
@@ -7,6 +7,50 @@ const Showcase = () => {
           <img src="/mask-logo.svg" />
         </div>
       </div>
+      <div className="content">
+        <div className="wrapper">
+          <div className="lg:max-w-md">
+            <h2>Rocket Chip</h2>
+
+            <div className="space-y-5 mt-7 pe-10">
+              <p>
+                Introducing{' '}
+                <span className="text-white">
+                  M4, the next generation of Apple silicon
+                </span>
+                . M4 powers
+              </p>
+              <p>
+                It drives Apple Intelligence on iPad Pro, so you can write,
+                create, and accomplish more with ease. All in a design that’s
+                unbelievably thin, light, and powerful.
+              </p>
+              <p>
+                A brand-new display engine delivers breathtaking precision,
+                color accuracy, and brightness. And a next-gen GPU with
+                hardware-accelerated ray tracing brings console-level graphics
+                to your fingertips.
+              </p>
+              <p className="text-primary">
+                Learn more about Apple Intelligence
+              </p>
+            </div>
+          </div>
+
+          <div className="max-w-3xs space-y-14">
+            <div className="space-y-2">
+              <p>Up to</p>
+              <h3>4x faster</h3>
+              <p>pro rendering performance than M2</p>
+            </div>
+            <div className="space-y-2">
+              <p>Up to</p>
+              <h3>1.5x faster</h3>
+              <p>CPU performance than M2</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
