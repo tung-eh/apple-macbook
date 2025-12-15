cd0a8f87a2a764bdc7295bd87f198172bb80a278
Tung Teng <thetungctn@gmail.com>
2025-12-15 17:02:48 +0700

    Spin the macbook on scroll

--- src/App/Features/ModelScroll.tsx
+++ src/App/Features/ModelScroll.tsx
@@ -1,6 +1,8 @@
 import { useRef, Suspense } from 'react'
 import { Html } from '@react-three/drei'
 import * as THREE from 'three'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
 import { useMediaQuery } from 'react-responsive'
 
 import Macbook from './Macbook'
@@ -9,6 +11,22 @@ const ModelScroll = () => {
   const groupRef = useRef<THREE.Group>(null)
 
   const isMobile = useMediaQuery({ query: '(max-width: 1024px)' })
+
+  useGSAP(() => {
+    if (groupRef.current) {
+      gsap.to(groupRef.current.rotation, {
+        y: Math.PI * 2,
+        ease: 'power1.inOut',
+        scrollTrigger: {
+          trigger: '#f-canvas',
+          start: 'top top',
+          end: 'bottom  top',
+          scrub: 1,
+          pin: true,
+        },
+      })
+    }
+  })
 
   return (
     <group ref={groupRef}>
