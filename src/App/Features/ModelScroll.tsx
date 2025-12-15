cc49e965c163c946571acfa60f561c29abd9da14
Tung Teng <thetungctn@gmail.com>
2025-12-15 23:13:29 +0700

    Animate features content

--- src/App/Features/ModelScroll.tsx
+++ src/App/Features/ModelScroll.tsx
@@ -5,12 +5,15 @@ import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { useMediaQuery } from 'react-responsive'
 
+import useMacbookStore from './store'
 import Macbook from './Macbook'
 
 const ModelScroll = () => {
   const groupRef = useRef<THREE.Group>(null)
 
   const isMobile = useMediaQuery({ query: '(max-width: 1024px)' })
+
+  const { texture, setTexture } = useMacbookStore()
 
   useGSAP(() => {
     if (groupRef.current) {
@@ -25,6 +28,30 @@ const ModelScroll = () => {
           pin: true,
         },
       })
+
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: '#f-canvas',
+            start: 'top center',
+            end: 'bottom  top',
+            scrub: 1,
+          },
+        })
+        .call(() => setTexture('/videos/feature-1.mp4'))
+        .to('.box1', { opacity: 1, y: 0, delay: 1 })
+
+        .call(() => setTexture('/videos/feature-2.mp4'))
+        .to('.box2', { opacity: 1, y: 0 })
+
+        .call(() => setTexture('/videos/feature-3.mp4'))
+        .to('.box3', { opacity: 1, y: 0 })
+
+        .call(() => setTexture('/videos/feature-4.mp4'))
+        .to('.box4', { opacity: 1, y: 0 })
+
+        .call(() => setTexture('/videos/feature-5.mp4'))
+        .to('.box5', { opacity: 1, y: 0 })
     }
   })
 
@@ -33,14 +60,14 @@ const ModelScroll = () => {
       <Suspense
         fallback={
           <Html>
-            <h1 className="text-white text-3xl uppercase">Loading...</h1>
+            <h1 className="text-white text-2xl uppercase">Loading...</h1>
           </Html>
         }
       >
         <Macbook
           scale={isMobile ? 0.05 : 0.08}
           position={[0, -1, 0]}
-          texturePath="/videos/feature-1.mp4"
+          texturePath={texture}
         />
       </Suspense>
     </group>
